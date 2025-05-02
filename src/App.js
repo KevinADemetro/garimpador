ec3f2c9547ccc61fdc1d3eabcadc6c939ea2f9fa
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-02 20:59:07 -0300

    Adicionando produto na tabela

--- src/App.js
+++ src/App.js
@@ -1,104 +1,98 @@
 import { useState } from "react";
 
-const produtosFake = [
-  {
-    nome: "Curso de Inglês Online",
-    rangeCpc: "R$0,60 - R$4,20",
-    preco: "R$197,00",
-    comissao: "R$98,50",
-    qtdBuscasMes: "12.000",
-    cpcMedio: "R$1,50",
-    percentualComissao: "50%",
-    custoVenda: "R$15,00",
-    lucro: "R$83,50",
-    cpcMaximo: "R$1,90",
-  },
-  {
-    nome: "Suplemento Natural para Ansiedade",
-    rangeCpc: "R$0,80 - R$5,50",
-    preco: "R$147,00",
-    comissao: "R$73,50",
-    qtdBuscasMes: "9.500",
-    cpcMedio: "R$1,70",
-    percentualComissao: "50%",
-    custoVenda: "R$18,00",
-    lucro: "R$55,50",
-    cpcMaximo: "R$2,10",
-  },
-  {
-    nome: "E-book de Receitas Fit",
-    rangeCpc: "R$0,40 - R$2,80",
-    preco: "R$49,90",
-    comissao: "R$34,93",
-    qtdBuscasMes: "5.000",
-    cpcMedio: "R$0,90",
-    percentualComissao: "70%",
-    custoVenda: "R$7,00",
-    lucro: "R$27,93",
-    cpcMaximo: "R$1,10",
-  },
-  {
-    nome: "Curso de Edição de Vídeo com CapCut",
-    rangeCpc: "R$0,50 - R$3,50",
-    preco: "R$97,00",
-    comissao: "R$48,50",
-    qtdBuscasMes: "7.800",
-    cpcMedio: "R$1,20",
-    percentualComissao: "50%",
-    custoVenda: "R$10,00",
-    lucro: "R$38,50",
-    cpcMaximo: "R$1,50",
-  },
-  {
-    nome: "Guia de Investimentos para Iniciantes",
-    rangeCpc: "R$0,70 - R$4,80",
-    preco: "R$159,00",
-    comissao: "R$79,50",
-    qtdBuscasMes: "6.300",
-    cpcMedio: "R$1,60",
-    percentualComissao: "50%",
-    custoVenda: "R$12,00",
-    lucro: "R$67,50",
-    cpcMaximo: "R$1,90",
-  },
-].map((produto) => ({
-  ...produto,
-  id: crypto.randomUUID(),
-}));
+const produtoDefault = {
+  nome: "",
+  rangeCpc: "",
+  preco: "",
+  comissao: "",
+  qtdBuscasMes: "",
+  cpcMedio: "",
+  percentualComissao: "",
+  custoVenda: "",
+  lucro: "",
+  cpcMaximo: "",
+};
 
 function App() {
-  const [produtos, setProdutos] = useState(produtosFake);
+  function handleAddProduto(produto) {
+    setProdutos((produtos) => [...produtos, produto]);
+  }
+  const [produtos, setProdutos] = useState([]);
   return (
     <>
-      <Formulario />
-      <TabelaProdutos produtos={produtos} />
+      <Formulario onAddProduto={handleAddProduto} />
+      {produtos.length > 0 && <TabelaProdutos produtos={produtos} />}
     </>
   );
 }
 
-function Formulario() {
-  const [produto, setProduto] = useState(
-    useState({
-      nome: "",
-      rangeCpc: "",
-      preco: "",
-      comissao: "",
-      qtdBuscasMes: "",
-      cpcMedio: "",
-      percentualComissao: "",
-      custoVenda: "",
-      lucro: "",
-      cpcMaximo: "",
-    })
-  );
+function Formulario({ onAddProduto }) {
+  const [produto, setProduto] = useState(produtoDefault);
 
   function handleChange(e) {
     const { name, value } = e.target;
     setProduto((prev) => ({ ...prev, [name]: value }));
   }
 
+  function calcularPercentualComissao(comissao, preco) {
+    const precoNumero = parseFloat(preco.replace("R$", "").replace(",", "."));
+    const comissaoNumero = parseFloat(
+      comissao.replace("R$", "").replace(",", ".")
+    );
+
+    if (!precoNumero || !comissaoNumero) return 0;
+    return (comissaoNumero / precoNumero) * 100;
+  }
+
+  function calcularCpcMedio(rangeCpc) {
+    const arrayCpc =
+      rangeCpc
+        .match(/[\d,]+/g)
+        ?.map((cpc) => parseFloat(cpc.replace(",", "."))) || [];
+
+    if (arrayCpc.length === 0) return "";
+
+    const soma = arrayCpc.reduce((acc, val) => acc + val, 0);
+    const media = soma / arrayCpc.length;
+
+    return media.toFixed(2);
+  }
+
+  function calcularCustoPorVenda(cpcMedio) {
+    return (cpcMedio * 30).toFixed(2);
+  }
+
+  function calcularLucro(custoVenda, comissao) {
+    return (comissao - custoVenda).toFixed(2);
+  }
+
+  function calcularCpcMaximo(comissao) {
+    return (comissao / 30).toFixed(2);
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const percentualComissao = calcularPercentualComissao(
+      produto.comissao,
+      produto.preco
+    );
+    const cpcMedio = calcularCpcMedio(produto.rangeCpc);
+    const custoVenda = calcularCustoPorVenda(cpcMedio);
+    const lucro = calcularLucro(custoVenda, produto.comissao);
+    const cpcMaximo = calcularCpcMaximo(produto.comissao);
+    const produtoAtualizado = {
+      ...produto,
+      id: crypto.randomUUID(),
+      percentualComissao: percentualComissao,
+      cpcMedio: cpcMedio,
+      custoVenda: custoVenda,
+      lucro: lucro,
+      cpcMaximo: cpcMaximo,
+    };
+
+    onAddProduto(produtoAtualizado);
+    setProduto(produtoDefault);
   }
 
   return (
@@ -133,48 +127,6 @@ function Formulario() {
             name="qtdBuscasMes"
           >
             Quantidade de Buscas Mês:
-          </Campo>
-        </div>
-        <div className="row-group">
-          <Campo
-            onChange={handleChange}
-            value={produto.cpcMedio}
-            name="cpcMedio"
-            readOnly={true}
-          >
-            CPC Médio:
-          </Campo>
-          <Campo
-            onChange={handleChange}
-            value={produto.percentualComissao}
-            name="percentualComissao"
-            readOnly={true}
-          >
-            Percentual Comissão:
-          </Campo>
-          <Campo
-            onChange={handleChange}
-            value={produto.custoVenda}
-            name="custoVenda"
-            readOnly={true}
-          >
-            Custo por Venda
-          </Campo>
-          <Campo
-            onChange={handleChange}
-            value={produto.lucro}
-            name="lucro"
-            readOnly={true}
-          >
-            Lucro
-          </Campo>
-          <Campo
-            onChange={handleChange}
-            value={produto.cpcMaximo}
-            name="cpcMaximo"
-            readOnly={true}
-          >
-            CPC Máximo
           </Campo>
         </div>
         <button>Adicionar</button>
@@ -220,14 +172,14 @@ function TabelaProdutos({ produtos }) {
         {produtos.map((produto) => (
           <tr key={produto.id}>
             <td className="txt-left">{produto.nome}</td>
-            <td>{produto.preco}</td>
-            <td>{produto.comissao}</td>
-            <td>{produto.custoVenda}</td>
-            <td>{produto.lucro}</td>
-            <td>{produto.percentualComissao}</td>
+            <td>R${Number(produto.preco).toFixed(2)}</td>
+            <td>R${Number(produto.comissao).toFixed(2)}</td>
+            <td>R${Number(produto.custoVenda).toFixed(2)}</td>
+            <td>R${Number(produto.lucro).toFixed(2)}</td>
+            <td>{produto.percentualComissao}%</td>
             <td>{produto.rangeCpc}</td>
-            <td>{produto.cpcMedio}</td>
-            <td>{produto.cpcMaximo}</td>
+            <td>R${Number(produto.cpcMedio).toFixed(2)}</td>
+            <td>R${Number(produto.cpcMaximo).toFixed(2)}</td>
             <td>{produto.qtdBuscasMes}</td>
           </tr>
         ))}
