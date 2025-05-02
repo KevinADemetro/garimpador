a6b26d1fd9f337e6c3cdd3754b8be4af2ecf5295
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-01 21:35:22 -0300

    criando formulário estático

--- src/App.js
+++ src/App.js
@@ -10,7 +10,48 @@ function Formulario() {
   return (
     <div className="formulario">
       <h1>Formulário Produto</h1>
-      <form></form>
+      <form>
+        <div className="row-group">
+          <label>
+            Nome:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Range CPC:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Preço:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Comissão:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Quantidade de Buscas Mês:
+            <input type="text" name="" />
+          </label>
+        </div>
+        <div className="row-group">
+          <label>
+            CPC Médio:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Percentual Comissão:
+            <input type="text" name="" />
+          </label>
+          <label>
+            Custo por Venda
+            <input type="text" name="" />
+          </label>
+          <label>
+            Lucro
+            <input type="text" name="" />
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
